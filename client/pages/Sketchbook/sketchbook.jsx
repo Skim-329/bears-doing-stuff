c2bef2ee1c6842b693bab57f5f0651e71b4d564a
Simon Kim <mrsimonkim@gmail.com>
2021-11-11 05:20:37 +0000

    added eraser functionality and text box - f3

--- client/pages/Sketchbook/sketchbook.jsx
+++ client/pages/Sketchbook/sketchbook.jsx
@@ -2,52 +2,62 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { BearLogo } from '../../components/header/bearlogo';
 import CanvasDraw from 'react-canvas-draw';
+import { DrawerOptions, EraserOptions } from './drawing-options';
 import './styles.css';
 
-// static defaultProps = {
-//     onChange: null
-//     loadTimeOffset: 5,
-//     lazyRadius: 30,
-//     brushRadius: 12,
-//     brushColor: "#444",
-//     catenaryColor: "#0a0302",
-//     gridColor: "rgba(150,150,150,0.17)",
-//     hideGrid: false,
-//     canvasWidth: 400,
-//     canvasHeight: 400,
-//     disabled: false,
-//     imgSrc: "",
-//     saveData: null,
-//     immediateLoading: false,
-//     hideInterface: false,
-//     gridSizeX: 25,
-//     gridSizeY: 25,
-//     gridLineWidth: 0.5,
-//     hideGridX: false,
-//     hideGridY: false
-//   };
+function renderText() {
+  const ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = 'black';
+  ctx.font = '48px serif';
+  ctx.fillText('Hello world', 100, 150);
+}
 
 export default class Sketchbook extends React.Component {
-  render() {
-    const prompts = [
-      'Draw a bear drinking tea.',
-      'Draw a bear riding a bicycle.',
-      'Draw a bear trying on shoes.',
-      'Draw a bear jumping on a trampoline.',
-      'Draw a bear juggling.',
-      'Draw a bear playing basketball.',
-      'Draw a bear preventing forest fires.',
-      'Draw a bear attending Learning Fuze.',
-      'Draw a bear reading a book.',
-      'Draw a bear eating sushi.',
-      'Draw a bear driving a car.',
-      'Draw a bear eating watermelons.',
-      'Draw a bear on vacation.',
-      'Draw a bear doing the laundry.',
-      'Draw a bear buying groceries.'
-    ];
-    const randomIndex = Math.floor(Math.random() * prompts.length);
+  constructor(props) {
+    super(props);
+    this.state = {
+      pen: { brushRadius: 1 },
+      prompts: [
+        'Draw a bear drinking tea.',
+        'Draw a bear riding a bicycle.',
+        'Draw a bear trying on shoes.',
+        'Draw a bear jumping on a trampoline.',
+        'Draw a bear juggling.',
+        'Draw a bear playing basketball.',
+        'Draw a bear preventing forest fires.',
+        'Draw a bear attending Learning Fuze.',
+        'Draw a bear reading a book.',
+        'Draw a bear eating sushi.',
+        'Draw a bear driving a car.',
+        'Draw a bear eating watermelons.',
+        'Draw a bear on vacation.',
+        'Draw a bear doing the laundry.',
+        'Draw a bear buying groceries.'
+      ]
+    };
+    this.handleSelectBrushRadius = this.handleSelectBrushRadius.bind(this);
+  }
 
+  componentDidMount() {
+    renderText();
+    this.setState(prevState => ({
+      randomIndex: Math.floor(Math.random() * prevState.prompts.length)
+    }));
+  }
+
+  handleSelectBrushRadius(event) {
+    const radius = Number(event.target.id);
+    this.setState({
+      pen: {
+        brushRadius: radius
+      }
+    });
+  }
+
+  render() {
+    const { pen, randomIndex, prompts } = this.state;
+    // renderText();
     return (
       <div className="sketchbook-content">
         <header className="sketchbook-header">
@@ -57,13 +67,22 @@ export default class Sketchbook extends React.Component {
           <h1>{prompts[randomIndex]}</h1>
         </header>
         <div>
-          <div className="sketchbook-sidebar"></div>
+          <div className="sketchbook-sidebar">
+            <div className="sketchbook-pen">
+              <DrawerOptions selectRadius={this.handleSelectBrushRadius} />
+            </div>
+            <div className="sketchbook-eraser">
+              <EraserOptions selectRadius={this.handleSelectBrushRadius} />
+            </div>
+          </div>
+          <textarea id="sketchbook-text-area" />
           <div className="sketchbook-canvas">
             <CanvasDraw
               lazyRadius={0}
-              canvasWidth={700}
-              canvasHeight={500}
-              brushRadius={1}
+              canvasWidth={900}
+              canvasHeight={600}
+              brushRadius={pen.brushRadius}
+              hideGrid={true}
             />
           </div>
         </div>
